eb1af0d838dadd6fd29a22beb81f1401a5c252dc
KeikoKato <keikokato0210@gmail.com>
2023-02-18 17:29:03 +0900

    week1 class1_7 Must課題1です
    よろしくお願いします

---
+++ Must1/Must1_GetMaxMin.js
@@ -0,0 +1,34 @@
+'use strict'
+
+// 1
+// コンストラクタに数値の配列を取り、最大値を取得するメソッド、最小値を取得するメソッドを持つクラスを作成してください。
+class GetMaxMin {
+    constructor(arr) {
+        this.arr = arr;
+        console.log(this.arr);
+    }
+
+    getMax() {
+        let result = this.arr[0];
+        for (const value of this.arr) {
+            if (value > result) {
+                result = value;
+            }
+        }
+        return result;
+    }
+
+    getMin() {
+        let result = this.arr[0];
+        for (const value of this.arr) {
+            if (value < result) {
+                result = value;
+            }
+        }
+        return result;
+    }
+
+};
+
+module.exports = new GetMaxMin([1, 2, 3]);
+
---
+++ Must1/Must1_GetMaxMin.test.js
@@ -0,0 +1,19 @@
+'use strict'
+
+const instance = require('./Must1_GetMaxMin.js');
+
+describe('GetMaxMin', () => {
+
+    test('[1, 2, 3]の最大値は3', () => {
+        const result = instance.getMax();
+        expect(result).toBe(3);
+    });
+
+    test('[1, 2, 3]の最小値は1', () => {
+        const result = instance.getMin();
+        expect(result).toBe(1);
+    });
+
+})
+
+
